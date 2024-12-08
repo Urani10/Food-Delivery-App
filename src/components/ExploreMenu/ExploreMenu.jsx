e89d9e45deb6be392791a0fe6225104e42afa829
unknown <uran.namani39@gmail.com>
2024-12-08 21:30:36 +0100

    fixed img and formated code

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./ExploreMenu.css";
 import { menu_list } from "../../assets/assets";
 
-const ExploreMenu = ({category,setCategory}) => {
+const ExploreMenu = ({ category, setCategory }) => {
   return (
     <div className="explore-menu" id="explore-menu">
       <h1>Explore our Menu</h1>
@@ -12,13 +12,25 @@ const ExploreMenu = ({category,setCategory}) => {
         one delicious meal at a time.
       </p>
       <div className="explore-menu-list">
-        {menu_list.map((item,index) =>{
-            return(
-                <div onClick={()=>setCategory(perv=>perv===item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item"> 
-                <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
-                <p>{item.menu_name}</p>
-                </div>
-            )
+        {menu_list.map((item, index) => {
+          return (
+            <div
+              onClick={() =>
+                setCategory((perv) =>
+                  perv === item.menu_name ? "All" : item.menu_name
+                )
+              }
+              key={index}
+              className="explore-menu-list-item"
+            >
+              <img
+                className={category === item.menu_name ? "active" : ""}
+                src={item.menu_image}
+                alt=""
+              />
+              <p>{item.menu_name}</p>
+            </div>
+          );
         })}
       </div>
       <hr />
